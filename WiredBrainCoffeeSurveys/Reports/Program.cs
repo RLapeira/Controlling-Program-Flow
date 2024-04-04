efc927ac4e0ef659f831ae57524dc7911c31c33f
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-04 10:48:34 +0200

    Demo: Touring a Finalized App State

--- WiredBrainCoffeeSurveys/Reports/Program.cs
+++ WiredBrainCoffeeSurveys/Reports/Program.cs
@@ -18,19 +18,18 @@ namespace WiredBrainCoffeeSurveys.Reports
                 Console.WriteLine("Please specify which quarter of data: (q1, q2)");
                 var selectedData = Console.ReadLine();
 
-                var surveyResults = JsonConverter.DeserializeObject<SurveyResults>
-                    (File.ReadAllText($"data/{selectedData}.json"));
+                var surveyResults = SurveyDataService.GetSurveyDataByFileName(selectedFile);
 
                 switch (selectedReport)
                 {
                     case "rewards":
-                        GenerateWinnerEmails(surveyResults);
+                        RewardsReportService.GenerateWinnerEmails(surveyResults);
                         break;
                     case "comments":
-                        GenerateTasksReport(surveyResults);
+                        CommentsReportService.GenerateCommentsReport(surveyResults);
                         break;
                     case "tasks":
-                        GenerateCommentsReport(surveyResults);
+                        TasksReportService.GenerateTasksReport(surveyResults);
                         break;
                     case "quit":
                         quitApp = true;
@@ -42,100 +41,6 @@ namespace WiredBrainCoffeeSurveys.Reports
                 Console.WriteLine();
             }
             while (!quitApp);
-        }
-
-        private static void GenerateWinnerEmails(SurveyResults results)
-        {
-            var selectedEmails = new List<string>();
-            int counter = 0;
-
-            while (selectedEmails.Count < 2 && counter < results.Responses.Count)
-            {
-                var currentItem = results.Responses[counter];
-
-                if (currentItem.FavoriteProduct == "Cappucino")
-                {
-                    selectedEmails.Add(currentItem.EmailAddress);
-                    Console.WriteLine(currentItem.EmailAddress);
-                }
-
-                counter++;
-            }
-
-            File.WriteAllLines("WinnersReport.csv", selectedEmails);
-        }
-
-        private static void GenerateCommentsReport(SurveyResults results)
-        {
-            for (var i = 0; i < results.Responses.Count; i++)
-            {
-                var currentResponse = results.Responses[i];
-
-                if (currentResponse.WouldRecommend < 7.0)
-                {
-                    Console.WriteLine(currentResponse.Comments);
-                }
-            }
-
-            foreach (var response in results.Responses)
-            {
-                if (response.AreaToImprove == results.AreaToImprove)
-                {
-                    if (response.AreaToImprove == results.AreaToImprove)
-                    {
-                        Console.WriteLine(response.Comments);
-                    }
-                }
-            }
-        }
-
-        public static void GenerateTasksReport(SurveyResults results)
-        {
-            var tasks = new List<string>();
-
-            // Calculated Values
-            double responseRate = results.NumberResponded / results.NumberSurveyed;
-            double overallScore = (results.ServiceScore + results.CoffeeScore
-                + results.FoodScore + results.PriceScore) / 4;
-
-            if (results.CoffeeScore > results.FoodScore)
-            {
-                tasks.Add("Investigate coffee recipes and ingredients.");
-            }
-
-            tasks.Add(overallScore > 8.0 ? "Work with leadership to reward staff" : "Work with employees for improvement ideas.");
-
-            if (overallScore > 8.0)
-            {
-                tasks.Add("Work with leadership to reward staff");
-            }
-            else
-            {
-                tasks.Add("Work with employees for improvement ideas.");
-            }
-
-            tasks.Add(responseRate switch
-            {
-                var rate when rate < .33 => "Research options to improve response rate.",
-                var rate when rate > .33 && rate < .66 => "Reward participants with free coffee coupon.",
-                var rate when rate > .66 => "Reward participants with discount coffee coupon."
-            });
-
-            tasks.Add(results.AreaToImprove switch
-            {
-                "RewardsProgram" => "Revisit the rewards deals.",
-                "Cleanliness" => "Contact the cleaning vendor.",
-                "MobileApp" => "Contact consulting firm about app.",
-                _ => "Investigate individual comments for ideas."
-            });
-
-            Console.WriteLine(Environment.NewLine + "Tasks Output:");
-            foreach (var task in tasks)
-            {
-                Console.WriteLine(task);
-            }
-
-            File.WriteAllLines("TasksReport.csv", tasks);
         }
     }
 }
